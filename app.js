16e85d799a4b266454f8c7df45ca9a6b7e8de710
richardgarcia <richgarcia7525@gmail.com>
2023-05-03 14:40:51 -0500

    untracked node and added gitignore file.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+//importing of the package
 const express = require('express');
 
+//execution of the package
 const app = express();
 
-//ROUTES
-app.get('/',  (req, res) => {
-    res.send('We are on home');
-})
 
+const mongoose = require('mongoose');
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts');
+});
+
+
+
+//Connect to Db
+mongoose.connect('mongodb+srv://richgarcia7525:<DanMarkson308>@restapi.hfobuse.mongodb.net/?retryWrites=true&w=majority', () => console.log('connected to DB!')
+);
 //This is how we can start listening to our server
 app.listen(3000);
